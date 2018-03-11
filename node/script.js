03c54e8d530fe27f109fbb99bc92342f010d6b3b
Nathanaël Lécaudé <natcl@users.noreply.github.com>
2018-03-11 19:19:03 -0400

    Add arguments printing

--- node/script.js
+++ node/script.js
@@ -1,2 +1,4 @@
 console.log('hello from nodejs !');
+console.log('Here are the supplied arguments');
+console.log(process.argv);
 setInterval(x => {}, 1000);
--- python/script.py
+++ python/script.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import time
+import sys
 
 print('Hello from python !')
+print("Here are the supplied arguments:")
+print(sys.argv)
 while 1:
     time.sleep(1000)
